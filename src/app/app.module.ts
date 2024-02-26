997d046f48102803efe88e247c5f67d1947e11c4
Firas <firas.abdallah@esprit.tn>
2024-02-26 23:59:38 +0100

    template front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { ElderComponent } from './elder/elder.component';
+import { NurseComponent } from './nurse/nurse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    WelcomePageComponent,
+    DoctorComponent,
+    ElderComponent,
+    NurseComponent
   ],
   imports: [
     BrowserModule,
